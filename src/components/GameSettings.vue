0f6f8cb8d963ae8b111e7eb6094574d522421c33
Raphael Aleixo <raleixo@lumis.com.br>
2019-08-21 17:05:08 -0300

    Initial commit

---
+++ src/components/GameSettings.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="createNewGame"
+    class="settings">
+    <v-combobox
+      v-model="newgame.numberOfPlayers"
+      :items="players"
+      label="Players"
+    />
+    <v-combobox
+      v-model="newgame.base"
+      :items="boardSizes"
+      label="Board size"
+    />
+    <v-btn
+      large
+      color="primary">Play now</v-btn>
+  </form>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+  import { mapState } from 'vuex';
+  export default {
+    data () {
+      return {
+        newgame: {
+          numberOfPlayers: 2,
+          base: {text:'Very small', value:6}
+        },
+        players: [2, 3, 4],
+        boardSizes: [{text:'Very small', value:6}, {text:'Small', value: 7}, {text:'Medium', value: 8}, {text:'Large', value: 9}, {text:'Very large', value: 10}]
+      }
+    }
+  }
+</script>
